0caed8ebd9fc3f98c6b471952a9550eb7566b81a
duboisel <eliott.dubois@cea.fr>
2025-05-14 22:23:55 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import glob
 
-from setuptools.command import build_ext
 import setuptools
 
 
@@ -14,43 +13,15 @@ class get_pybind_include:
         return pybind11.get_include(self.user)
 
 
-# class custom_build_ext(build_ext):
-#     def build_extensions(self):
-#         # Override the compiler executables. Importantly, this
-#         # removes the "default" compiler flags that would
-#         # otherwise get passed on to to the compiler, i.e.,
-#         # distutils.sysconfig.get_var("CFLAGS").
-#         self.compiler.set_executable("compiler_so", "c++")
-#         self.compiler.set_executable("compiler_cxx", "c++")
-#         self.compiler.set_executable("linker_so", "c++")
-#         build_ext.build_extensions(self)
-
-
 depmod_cpp_module = setuptools.Extension(
     "depmod._lib",
     glob.glob("src/cpp/*.cpp"),
-    include_dirs=[
-        "src/cpp/",
-        "external/eigen/",
-        get_pybind_include(),
-        get_pybind_include(True),
-    ],
+    include_dirs=["src/cpp/", "external/eigen/", get_pybind_include(), get_pybind_include(True)],
     language="c++",
-    extra_compile_args=[
-        "-std=c++2a",
-        "-O3",
-        "-flto",
-        "-fPIC",
-        "-Wall",
-        "-Wextra",
-        "-Wattributes",
-    ],
+    extra_compile_args=["-std=c++2a", "-O3", "-flto", "-fPIC", "-Wall", "-Wextra", "-Wattributes"],
     extra_link_args=[],
 )
 
 
 if __name__ == "__main__":
-    setuptools.setup(
-        ext_modules=[depmod_cpp_module],
-        zip_safe=False,
-    )
+    setuptools.setup(ext_modules=[depmod_cpp_module], zip_safe=False)
