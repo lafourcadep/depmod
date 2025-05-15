aafe405904b78b3ad71557f270240b336af09dcb
duboisel <eliott.dubois@cea.fr>
2025-05-15 02:00:14 +0200

    update examples

--- examples/traction_isoV/main.py
+++ examples/traction_isoV/main.py
@@ -1,50 +1,20 @@
-import warnings
-warnings.filterwarnings("ignore")
-
-from depmod.core import *
-from depmod.config import init_config
-from depmod.deformation import Traction
-from depmod.io import read_atom
-from depmod.units import convert
+from depmod import DeformationPath, Traction, read_lattice
+from depmod.core import lmp
 
 if __name__ == "__main__":
 
-    # define the deformation
-    # unixial traction in the x direction
-    deform = Traction(isoV=True).from_axis([1, 0, 0])
-
-    # define the simulation parameters
-    config = init_config(
-        gammadot= 1e10,
-        t_max=200e-12,
-        npts=200,
-        kpts=5000,
-    )
-
-    # Read lattice from lammps file
-    atoms = read_atom("dir.input/Cu_fcc_5x5x5.lmp.gz")
-
-    # Generate box parameters time evolution
-    box_evolution_data = lammps_generate_box_evolution_data(
-        atoms.cell,
-        config,
-        deform,
-        method="brute",
-        filename="dir.depmod/box_evolution_data.csv"
-    )
-
-    # Write lammps deformation module
-    lammps_write_deformation_module(
-        box_evolution_data,
+    lmp.write_deformation_module(
+        # Generate the time evolution of the box parameter
+        lmp.generate_box_evolution_data(
+            # read H(0) from lammps file
+            lattice=read_lattice("./dir.input/Cu_fcc_5x5x5.lmp.gz"),
+            # Create a deformation path: Uni-axial isochoric traction
+            deformation=DeformationPath(
+                Traction.from_axis([1, 0, 0], isoV=True), strain_rate=1e10, tmax=200e-12, npts=200
+            ),
+            filename="dir.depmod/box_evolution_data.csv",
+        ),
         mod_file="dir.depmod/lmp_fix_deform.mod",
         fit_file="dir.depmod/box_evolution_fit.csv",
-        poly_file="dir.depmod/poly_coeffs.csv"
+        poly_file="dir.depmod/poly_coeffs.csv",
     )
-
-    # Write exastamp deformation module
-    # exastamp_write_deformation_module(
-    #     exastamp_generate_box_evolution_data(
-    #         config = config,
-    #         deform = deform
-    #     )
-    # )
